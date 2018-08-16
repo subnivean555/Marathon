f5f6bc67add2da3b45d340af523c2e6536a71e6c
weilan <xiao282020930@gmail.com>
2018-08-16 20:45:48 +0800

    2018-08-16

---
+++ src/algoAndStructure/sort/ShellSort.java
@@ -0,0 +1,29 @@
+package algoAndStructure.sort;
+
+public class ShellSort {
+
+    public static void main(String[] args) {
+        Integer[] arr = SortTestHelper.generateRandomArray(100000, 0, 100000);
+        //Integer[] arr = SortTestHelper.generateNearlyOrderedArray(10000, 20);
+        Long begin = System.currentTimeMillis();
+        sort(arr, arr.length);
+        System.out.println("shell sort 执行的时间为 : " + (System.currentTimeMillis() - begin));
+
+    }
+
+    public static void sort(Comparable[] arr, int len){
+        int h = 1;
+        while (h < len / 3){
+            h = 3 * h + 1;
+        }
+        while (h >= 1){
+            for (int i = h; i < len; i++){
+                for (int j = i; j >= h && SortTestHelper.less(arr[j], arr[j - h]); j -= h){
+                    SortTestHelper.exch(arr, j, j-h);
+                }
+            }
+            h /= 3;
+        }
+        assert SortTestHelper.isSorted(arr);
+    }
+}
---
+++ src/algoAndStructure/sort/SortTestHelper.java
@@ -0,0 +1,62 @@
+package algoAndStructure.sort;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class SortTestHelper {
+
+    public static Integer[] generateNearlyOrderedArray(int len, int swapTimes){
+        Integer[] arr = new Integer[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = i;
+        }
+        Random rand = new Random();
+        for (int i = 0; i < swapTimes; i++){
+            int j = rand.nextInt(len);
+            int k = rand.nextInt(len);
+            int temp = arr[k];
+            arr[k] = arr[j];
+            arr[j] = temp;
+        }
+        return arr;
+    }
+
+    public static Integer[] generateRandomArray(int len, int left, int right){
+
+        assert (right > left);
+
+        Integer[] arr = new Integer[len];
+        Random random = new Random(System.currentTimeMillis());
+        for (int i = 0; i < len; i++){
+            arr[i] = random.nextInt() % (right - left + 1) + left;
+        }
+        return arr;
+    }
+
+    public static void print(Comparable[] arr){
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static boolean isSorted(Comparable[] arr) {
+        return isSorted(arr, 0, arr.length);
+    }
+
+    public static boolean isSorted(Comparable[] arr, int begin, int end) {
+        for (int i = begin + 1; begin < end; i++){
+            if (less(arr[i], arr[i - 1])){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    public static void exch(Comparable[] arr, int i, int j) {
+        Comparable temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
